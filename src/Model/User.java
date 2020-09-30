de19332b6e3bf15e9c261b1139a838f13fb6e220
user <justinmarar@gmail.com>
2020-09-30 11:31:26 +0200

    [ADD] Java-json Package hinzugefügt

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,4 +1,22 @@
 package Model;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 public class User {
+    public int     id;
+    public String  username;
+    public String  passwort;
+    public String  email;
+    public Boolean activated;
+    public User(JSONObject Data){
+        try{
+            this.id        = Data.getInt("id");
+            this.username  = Data.getString("id");
+            this.passwort  = Data.getString("passwort");
+            this.email     = Data.getString("email");
+            this.activated = Data.getBoolean("activated");
+        }
+        catch(JSONException e){
+            System.out.print("Fehler im JSON");
+        }
+    }
 }
