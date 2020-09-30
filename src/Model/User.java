e08fa9147ff0cea7df9a2af7e1f69fac8720c08d
feraz98 <justinmarar@gmail.com>
2020-09-30 12:55:41 +0200

    [ADD] Project Klasse erstellt

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,25 +1,39 @@
 package Model;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URLConnection;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class User {
     public int     id;
     public String  username;
     public String  passwort;
     public String  email;
     public Boolean activated;
-    public User(JSONObject Data){
-        try{
-            this.id        = Data.getInt("id");
-            this.username  = Data.getString("id");
-            this.passwort  = Data.getString("passwort");
-            this.email     = Data.getString("email");
-            this.activated = Data.getBoolean("activated");
-        }
-        catch(JSONException e){
-            System.out.print("Fehler im JSON");
-        }
+    public User(int id){
+            this.id = id;
     }
-    public static void getUserById(int id){
-        //return new User();
+    public static void getJson(){
+        try{
+            URL httpClient = new URL("https","cloudu24.de/api/1.0/",443,"project/view?user_id=1");
+            URLConnection httppost = httpClient.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    httppost.getInputStream()));
+            String inputLine;
+            while ((inputLine = reader.readLine()) != null)
+                System.out.println(inputLine);
+            reader.close();
+        }
+        catch(java.net.MalformedURLException e){
+            System.out.print("Client funktioniert nicht");
+        }
+        catch(java.io.IOException f){
+            System.out.print("Verbindung funkioniert nicht");
+        }
     }
 }
--- src/start.java
+++ src/start.java
@@ -1,4 +1,5 @@
 public class start {
-    public static void main(){
+    public static void main(String[] args){
+        System.out.print("sdfgdfsg");
     }
 }
