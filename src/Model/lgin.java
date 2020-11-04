2dcc2f867c03c0ed85d58275f3ec4ced975dfa7e
user <justinmarar@gmail.com>
2020-11-04 12:18:15 +0100

    Added gui logen form

---
+++ src/Model/lgin.java
@@ -0,0 +1,14 @@
+package Model;
+
+import javax.swing.*;
+
+public class lgin {
+    private JPanel panel1;
+    private JButton loginButton;
+    private JTextField userNameTextField;
+    private JPasswordField a3e3ePasswordField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
